883e3afddd07aed8074e7c6d1189775c0f893caa
Rostyslav Mandybur <mandyburostyk@gmail.com>
2021-01-08 08:17:51 +0200

    added two catch blocks

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -19,12 +19,19 @@ public class ConsoleHandler {
             Bet bet = null;
             try {
                 String[] betData = command.split(" ");
+                if (betData.length > 2) {
+                    throw new RuntimeException();
+                }
                 int value = Integer.parseInt(betData[0]);
                 double risk = Double.parseDouble(betData[1]);
                 bet = new Bet(value, risk);
 
             } catch (NumberFormatException e) {
                 System.out.println("Incorrect input, please enter again");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("You need to input two values!");
+            } catch (RuntimeException e) {
+                System.out.println("Please, enter two values");
             }
             betDao.add(bet);
             System.out.println(bet == null ? null : bet.toString());
