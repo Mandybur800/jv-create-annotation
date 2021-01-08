c123c9e54951650d39586f70ec1f7597314ce0fd
Rostyslav Mandybur <mandyburostyk@gmail.com>
2021-01-08 10:57:41 +0200

    `game` was added to handler

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -2,16 +2,70 @@ package core.basesyntax.controller;
 
 import core.basesyntax.dao.BetDao;
 import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.dao.GameDao;
+import core.basesyntax.dao.GameDaoImpl;
 import core.basesyntax.model.Bet;
+import core.basesyntax.model.Game;
 import java.util.Scanner;
 
 public class ConsoleHandler {
+    private static final Scanner scanner = new Scanner(System.in);
     private BetDao betDao = new BetDaoImpl();
+    private GameDao gameDao = new GameDaoImpl();
 
     public void handle() {
-        Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Please, enter value and risk for your bet or 'q' for exit");
+            System.out.println("Please, enter command:\n"
+                    + "q - for exit from program;\n"
+                    + "1 - for input Bet;\n"
+                    + "2 - for input Game;\n");
+            String command = scanner.nextLine();
+            switch (command) {
+                case "q" :
+                    return;
+                case "1" :
+                    handleBet();
+                    break;
+                case "2" :
+                    handleGame();
+                    break;
+                default:
+                    System.out.println("Incorrect operation, please try again.");
+            }
+        }
+    }
+
+    private void handleGame() {
+        while (true) {
+            System.out.println("Please, enter type of sport or 'q' for exit to previous menu");
+            String sport = scanner.nextLine();
+            while (!sport.matches("[a-zA-Z_]\\w{2,}")) {
+                System.out.println("Please, enter correct sport or 'q' for exit to previous menu");
+                sport = scanner.nextLine();
+                if (sport.equalsIgnoreCase("q")) {
+                    return;
+                }
+            }
+            String team1 = "";
+            while (!team1.matches("[a-zA-Z_]\\w{2,}")) {
+                System.out.println("Please, enter first team");
+                team1 = scanner.nextLine();
+            }
+            String team2 = "";
+            while (!team2.matches("[a-zA-Z_]\\w{2,}")) {
+                System.out.println("Please, enter second team");
+                team2 = scanner.nextLine();
+            }
+            Game game = new Game(sport, team1, team2);
+            gameDao.add(game);
+            System.out.println(game);
+        }
+    }
+
+    private void handleBet() {
+        while (true) {
+            System.out.println("Please, enter value and risk for your bet "
+                    + "or 'q' for exit to previous menu");
             String command = scanner.nextLine();
             if (command.equalsIgnoreCase("q")) {
                 return;
